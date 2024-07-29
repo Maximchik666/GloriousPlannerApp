864896637fa9b4f85d21f57be5acac7bb4849fb1
Vladislav Meleshko <v.meleshko@kulu.com>
2024-07-29 20:57:37 +0400

    Initial Commit

---
+++ GloriousPlannerApp/GloriousPlannerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GloriousPlannerAppApp.swift
+//  GloriousPlannerApp
+//
+//  Created by Maksim Kruglov on 29.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct GloriousPlannerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GloriousPlannerAppTests/GloriousPlannerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GloriousPlannerAppTests.swift
+//  GloriousPlannerAppTests
+//
+//  Created by Maksim Kruglov on 29.07.2024.
+//
+
+import Testing
+@testable import GloriousPlannerApp
+
+struct GloriousPlannerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
